15089492b01e2389e542793f10b8b23bdcf1bfad
iosghost1 <ledzledio@gmail.com>
2026-04-30 23:04:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-import threading
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -10,18 +9,12 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
-# TELEGRAM BOT
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is ON 🔥")
 
-def run_bot():
+if __name__ == "__main__":
     TOKEN = os.getenv("BOT_TOKEN")
     app_bot = ApplicationBuilder().token(TOKEN).build()
     app_bot.add_handler(CommandHandler("start", start))
+    
     app_bot.run_polling()
-
-# RUN BOTH
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port)

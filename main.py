2188e128927c0eb5f98ec5259eef69820315ec6a
iosghost1 <ledzledio@gmail.com>
2026-05-01 14:28:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import os
 import random
 import string
-import logging
 from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-# Loget
-logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -15,19 +11,19 @@ async def code(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
     await update.message.reply_text(f"Your code is: {new_code} 🔑")
 
+# KJO ZHDUK GABIMIN 404
 @app.route('/')
-def home():
-    return "Bot is Alive!", 200
+def index():
+    return "Bot is Alive! ✅"
 
 def main():
     TOKEN = os.environ.get("BOT_TOKEN")
-    URL = "https://onrender.com"
+    URL = "https://telegram-bot-mpya.onrender.com"
     PORT = int(os.environ.get("PORT", 10000))
 
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler("code", code))
 
-    # Ky rresht e mban botin ndezur dhe i pergjigjet Render-it
     application.run_webhook(
         listen="0.0.0.0",
         port=PORT,

cf71b30312e20493e1476873237291fd8b9b3d79
iosghost1 <ledzledio@gmail.com>
2026-05-01 02:32:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,10 @@ async def code(update: Update, context: ContextTypes.DEFAULT_TYPE):
     valid_codes.add(new_code)
     await update.message.reply_text(f"Your code is: {new_code} 🔑")
 
-# build bot
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("code", code))
 
-# run webhook
 if __name__ == "__main__":
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        url_path=TOKEN,
-        webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
-    )
+    app.run_polling()

f77534a6cdfe05ef7e689bb8d831f1f1bd98385e
iosghost1 <ledzledio@gmail.com>
2026-05-01 09:38:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import random
 import string
-import asyncio
 from flask import Flask, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -10,6 +9,7 @@ app = Flask(__name__)
 
 valid_codes = set()
 
+# command /code
 async def code(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
     valid_codes.add(new_code)
@@ -21,17 +21,21 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 bot_app = ApplicationBuilder().token(TOKEN).build()
 bot_app.add_handler(CommandHandler("code", code))
 
+# webhook route
 @app.route(f"/{TOKEN}", methods=["POST"])
-async def webhook():
+def webhook():
     update = Update.de_json(request.get_json(force=True), bot_app.bot)
-    await bot_app.process_update(update)
+    bot_app.process_update(update)
     return "ok"
 
+# test route
 @app.route("/")
 def home():
     return "Server is running ✅"
 
+# START
 if __name__ == "__main__":
+    import asyncio
     loop = asyncio.get_event_loop()
     loop.run_until_complete(bot_app.initialize())
     loop.run_until_complete(
